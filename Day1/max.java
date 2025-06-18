d781a01fc012afa0abdc2599275e216f0d716ddb
Achyuta41 <Achyutak41@gmail.com>
2025-06-18 21:27:09 +0530

    adding the maximum element finding

---
+++ Day1/max.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class max{
+	public static void main(String[] args)
+	{
+		Scanner inp=new Scanner(System.in);
+		int [] arr=new int[20];
+		System.out.println("Enter the size of the array:");
+		int size=inp.nextInt();
+		System.out.println("Enter array Element:");
+		int k=0;
+		for(int i=0;i<size;i++)
+		{
+			arr[i]=inp.nextInt();
+			if(k<arr[i])
+				k=arr[i];
+			
+		}
+		System.out.println("The Maximum Element:"+ k);
+	}
+}
